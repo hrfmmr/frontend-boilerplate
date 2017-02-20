0c220272b65acd68be07de3382103e1918f7a1e5
p_t <ladiciushm@gmail.com>
2017-02-21 00:16:38 +0900

    Add throws test

--- src/js/utils/divided.es6
+++ src/js/utils/divided.es6
@@ -1,4 +1,7 @@
 /* @flow */
-export function calculate(num: number): number {
+export default function calculate(num: number): number {
+  if (typeof num !== 'number' || isNaN(num)) {
+    throw new TypeError('Type of numeric is expected.')
+  }
   return Math.floor(num / 2)
 }
--- test/test-foo.js
+++ test/test-foo.js
@@ -1,6 +1,6 @@
 /* @flow */
 import test from 'ava'
-import { calculate } from '../src/js/utils/divided.es6'
+import calculate from '../src/js/utils/divided.es6'
 
 test('divided#calculate returns 2 when the value is 4', (t) => {
     t.is(calculate(4), 2)
@@ -13,4 +13,10 @@ test('divided#calculate returns 1 when the value is 3', (t) => {
      */
 
     t.is(calculate(3), 1);
+});
+
+test('divided#calculate throws exceptions when the value is other than numbers', t => {
+    t.throws(() => calculate(null), 'Type of numeric is expected.');
+    t.throws(() => calculate('abc'), / numeric /);
+    t.throws(() => calculate([]), TypeError, /^Type of numeric /);
 });
