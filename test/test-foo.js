092f9522600b32ec096ca57e82909626a5a6953b
p_t <ladiciushm@gmail.com>
2017-02-22 00:37:53 +0900

    Reformatting

--- test/test-foo.js
+++ test/test-foo.js
@@ -1,26 +1,25 @@
-/* @flow */
 import test from 'ava'
 import calculate from '../src/js/utils/divided.es6'
 import promiseFn from '../src/js/utils/promiseFn.es6'
 
 test('divided#calculate returns 2 when the value is 4', (t) => {
-    t.is(calculate(4), 2)
+  t.is(calculate(4), 2)
 })
 
 test('divided#calculate returns 1 when the value is 3', (t) => {
 
-    /*
-     * t.is(calculate(3), 1.5);
-     */
+  /*
+  * t.is(calculate(3), 1.5)
+  */
 
-    t.is(calculate(3), 1);
-});
+  t.is(calculate(3), 1)
+})
 
 test('divided#calculate throws exceptions when the value is other than numbers', (t) => {
-    t.throws(() => calculate(null), 'Type of numeric is expected.')
-    t.throws(() => calculate('abc'), / numeric /);
-    t.throws(() => calculate([]), TypeError, /^Type of numeric /);
-});
+  t.throws(() => calculate(null), 'Type of numeric is expected.')
+  t.throws(() => calculate('abc'), / numeric /)
+  t.throws(() => calculate([]), TypeError, /^Type of numeric /)
+})
 
 test('async function', async (t) => {
   const value = await promiseFn()
